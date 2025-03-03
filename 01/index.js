713a1909d10ab2acbbaf5833b6a6014aca2b78eb
s.gagulin <s.gagulin@spider.ru>
2025-03-03 23:34:21 +0300

    The first task solution has been added

--- 01/index.js
+++ 01/index.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 
+const fs = require('fs')
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
 const readline = require('node:readline');
 const { stdin: input, stdout: output } = require('node:process');
 
@@ -13,15 +16,38 @@ function randomInteger(min, max) {
 const currentValue = randomInteger(1, 2);
 
 rl.question('Добро пожаловать в Орел или решка! 1 или 2?', (answer) => {
+    const argv = yargs(hideBin(process.argv)).argv;
+    const file = argv._.length ? `${argv._[0]}.txt` : 'defaultLog.txt';
+
     if (currentValue === 1) {
-        Number(answer) === currentValue ?
-            console.log(`Победа! Правильный ответ: 1`) :
-            console.log(`Мимо! :( Правильный ответ: 1`);
+        if (Number(answer) === currentValue) {
+            const content = `${new Date()}: Победа \n`
+            fs.appendFile(file, content, 'utf8', (err) => {
+                if (err) throw err;
+                console.log('Победа! Правильный ответ: 1')
+            })
+        } else {
+            const content = `${new Date()}: Поражение \n`
+            fs.appendFile(file, content, 'utf8', (err) => {
+                if (err) throw err;
+                console.log('Мимо! :(  Правильный ответ: 1')
+            })
+        }
     }
     if (currentValue === 2) {
-        Number(answer) === currentValue ?
-            console.log(`Победа! Правильный ответ: 2`) :
-            console.log(`Мимо! :( Правильный ответ: 2`);
+        if (Number(answer) === currentValue) {
+            const content = `${new Date()}: Победа \n`
+            fs.appendFile(file, content, 'utf8', (err) => {
+                if (err) throw err;
+                console.log('Победа! Правильный ответ: 2')
+            })
+        } else {
+            const content = `${new Date()}: Поражение \n`
+            fs.appendFile(file, content, 'utf8', (err) => {
+                if (err) throw err;
+                console.log('Мимо! :(  Правильный ответ: 2')
+            })
+        }
     }
     rl.close();
 });
